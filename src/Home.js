053765a08b340ab2a754684b914ee0521bfea265
shehab124 <chehab.gamal@gmail.com>
2023-10-10 18:14:44 +0300

    Section-7

--- src/Home.js
+++ src/Home.js
@@ -2,20 +2,25 @@ import { useState } from "react";
 
 const Home = () => {
 
-    const [name, setName] = useState('CHEHAB');
-
-    const [age, setAge] = useState(25);
-
-    const handleClick = () => {
-        setName('chehab');
-        setAge(30);
+    const handleClick = (e) =>
+    {
+        // e represents the event
+        console.log(e);
+        console.log("first button clicked");
     }
     
+    const handleClickAgain = (name, e) =>
+    {
+        console.log(e);
+        console.log("second button clicked ");
+        console.log(name);
+    }
+
     return ( 
         <div className="home">
             <h2>HomePage</h2>
-            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('chehab', e)}>Click me again</button>
         </div>
      );
 }
